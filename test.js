d5d0aabe8fe07eaad4a729208becee3051618b84
ADITYABHAVAR17 <adityabhavar17@gmail.com>
2024-07-11 14:15:40 +0530

    second commit

---
+++ test.js
@@ -0,0 +1,14 @@
+import assert from 'node:assert';
+import colorNameList from './index.js';
+
+// Function to get RGB value by color name
+function getColorByName(colorName) {
+    return colorNameList[colorName.toLowerCase()] || null;
+}
+
+
+// Example usage
+console.log(getColorByName('blue')); // [0, 0, 255]
+console.log(getColorByName('red')); // [255, 0, 0]
+console.log(getColorByName('unknownColor')); // null
+
